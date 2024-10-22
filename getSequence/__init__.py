a80c67e50fcb0b9efd9386f5c72f7bd568bf1042
alexholehouse <alex.holehouse@gmail.com>
2024-10-21 21:22:27 -0500

    PEP517 compliance; complete description coming in PR

--- getSequence/__init__.py
+++ getSequence/__init__.py
@@ -1,11 +1,8 @@
 """A CLI to get a uniprot sequence returned to terminal"""
 
 # Add imports here
-from .getseq import getseq, batch_getseq
+from getSequence.getseq import getseq, batch_getseq
 
-# Handle versioneer
-from ._version import get_versions
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
+
+# Handle version
+from ._version import __version__
